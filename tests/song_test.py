263091c4787c0561a2707d4be51d059c7209ef64
m6ttw <matthewrhyswilliams@live.com>
2020-11-27 18:24:31 +0000

    all song properties passing

--- tests/song_test.py
+++ tests/song_test.py
@@ -10,12 +10,15 @@ class TestSong(unittest.TestCase):
         self.song4 = Song("Ring of Fire", "Johnny Cash", "Country")
         self.song5 = Song("No Scrubs", "TLC", "R&B")
         self.song6 = Song("Fly Me to the Moon", "Frank Sinatra", "Jazz")
-        # self.song7 = Song()
-        # self.song8 = Song()
-        # self.song9 = Song()
+        self.song7 = Song("Anarchy in the U.K.", "Sex Pistols", "Punk")
+        self.song8 = Song("No Woman, No Cry", "Bob Marley and the Wailers", "Reggae")
+        self.song9 = Song("Wake Me Up", "Avicii", "Dance")
 
     def test_song_has_title(self):
         self.assertEqual("Juicy", self.song2.title)
 
     def test_song_has_artist(self):
         self.assertEqual("TLC", self.song5.artist)
+
+    def test_song_has_genre(self):
+        self.assertEqual("Reggae", self.song8.genre)
