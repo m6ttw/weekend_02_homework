d6dd10e18262dd2e4b3f781e2d63b4fc15279434
m6ttw <matthewrhyswilliams@live.com>
2020-11-27 21:08:40 +0000

    tidied up variable names

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,30 +6,30 @@ from src.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.song1 = Song("Mr. Brightside", "The Killers", "Rock")
-        self.song2 = Song("Juicy", "The Notorious B.I.G.", "Rap")
-        self.song3 = Song("Dancing Queen", "ABBA", "Pop")
-        self.song4 = Song("Ring of Fire", "Johnny Cash", "Country")
-        self.song5 = Song("No Scrubs", "TLC", "R&B")
-        self.song6 = Song("Fly Me to the Moon", "Frank Sinatra", "Jazz")
-        self.song7 = Song("Anarchy in the U.K.", "Sex Pistols", "Punk")
-        self.song8 = Song("No Woman, No Cry", "Bob Marley and the Wailers", "Reggae")
-        self.song9 = Song("Wake Me Up", "Avicii", "Dance")
+        self.song_1 = Song("Mr. Brightside", "The Killers", "Rock")
+        self.song_2 = Song("Juicy", "The Notorious B.I.G.", "Rap")
+        self.song_3 = Song("Dancing Queen", "ABBA", "Pop")
+        self.song_4 = Song("Ring of Fire", "Johnny Cash", "Country")
+        self.song_5 = Song("No Scrubs", "TLC", "R&B")
+        self.song_6 = Song("Fly Me to the Moon", "Frank Sinatra", "Jazz")
+        self.song_7 = Song("Anarchy in the U.K.", "Sex Pistols", "Punk")
+        self.song_8 = Song("No Woman, No Cry", "Bob Marley and the Wailers", "Reggae")
+        self.song_9 = Song("Wake Me Up", "Avicii", "Dance")
         self.room = Room("CC Caraoke Bar")
-        self.guest1 = Guest("Abbie")
-        self.guest2 = Guest("Bob")
-        self.guest3 = Guest("Carol")
-        self.guest4 = Guest("Dave")
-        self.guest5 = Guest("Emma")
-        self.guest6 = Guest("Fred")
+        self.guest_1 = Guest("Abbie")
+        self.guest_2 = Guest("Bob")
+        self.guest_3 = Guest("Carol")
+        self.guest_4 = Guest("Dave")
+        self.guest_5 = Guest("Emma")
+        self.guest_6 = Guest("Fred")
 
     def test_room_has_name(self):
         self.assertEqual("CC Caraoke Bar", self.room.name)
 
     def test_can_add_song_to_room(self):
-        self.room.add_song(self.song1)
+        self.room.add_song(self.song_1)
         self.assertEqual(1, self.room.song_count())
 
     def test_can_add_guest_to_room(self):
-        self.room.add_guest(self.guest1)
+        self.room.add_guest(self.guest_1)
         self.assertEqual(1, self.room.guest_count())
--- tests/song_test.py
+++ tests/song_test.py
@@ -4,13 +4,13 @@ from src.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        self.song1 = Song("Mr. Brightside", "The Killers", "Rock")
+        self.song_1 = Song("Mr. Brightside", "The Killers", "Rock")
 
     def test_song_has_title(self):
-        self.assertEqual("Mr. Brightside", self.song1.title)
+        self.assertEqual("Mr. Brightside", self.song_1.title)
 
     def test_song_has_artist(self):
-        self.assertEqual("The Killers", self.song1.artist)
+        self.assertEqual("The Killers", self.song_1.artist)
 
     def test_song_has_genre(self):
-        self.assertEqual("Rock", self.song1.genre)
+        self.assertEqual("Rock", self.song_1.genre)
