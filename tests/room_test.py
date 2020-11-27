a918d1e87348c73efa9578aed4a1e54ac401f78f
m6ttw <matthewrhyswilliams@live.com>
2020-11-27 18:32:55 +0000

    room has name passed

--- tests/room_test.py
+++ tests/room_test.py
@@ -5,4 +5,8 @@ from src.guest import Guest
 from src.song import Song
 
 class TestRoom(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.room = Room("CC Caraoke Bar", [], [])
+
+    def test_room_has_name(self):
+        self.assertEqual("CC Caraoke Bar", self.room.name)
