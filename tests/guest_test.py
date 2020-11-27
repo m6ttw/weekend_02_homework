d75b0c8fe08ee971420a2d194aa386349d5b2e2a
m6ttw <matthewrhyswilliams@live.com>
2020-11-27 16:40:51 +0000

    guests all have names

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -6,9 +6,17 @@ from src.song import Song
 class TestGuest(unittest.TestCase):
     def setUp(self):
         self.guest1 = Guest("Abbie")
+        self.guest2 = Guest("Bob")
+        self.guest3 = Guest("Carol")
+        self.guest4 = Guest("Dave")
+        self.guest5 = Guest("Emma")
+        self.guest6 = Guest("Fred")
 
     def test_guest1_has_name(self):
         self.assertEqual("Abbie", self.guest1.name)
 
-    # def test_guest1_wrong_name_fails(self):
+    # def test_guest_wrong_name_fails(self):
     #     self.assertEqual("Susan", self.guest1.name)
+
+    def test_guest4_has_name(self):
+        self.assertEqual("Dave", self.guest4.name)
