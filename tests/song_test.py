f75643438d43c6be7151fc3aa3697d44ce649f54
m6ttw <matthewrhyswilliams@live.com>
2020-11-27 17:44:35 +0000

    song title test passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -3,4 +3,16 @@ import unittest
 from src.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.song1 = Song("Mr. Brightside", "The Killers", "Rock")
+        self.song2 = Song("Juicy", "The Notorious B.I.G.", "Rap")
+        self.song3 = Song("Dancing Queen", "ABBA", "Pop")
+        self.song4 = Song("Ring of Fire", "Johnny Cash", "Country")
+        self.song5 = Song("No Scrubs", "TLC", "R&B")
+        self.song6 = Song("Fly Me to the Moon", "Frank Sinatra", "Jazz")
+        # self.song7 = Song()
+        # self.song8 = Song()
+        # self.song9 = Song()
+
+    def test_song_has_title(self):
+        self.assertEqual("Juicy", self.song2.title)
