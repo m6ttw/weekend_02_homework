31833f0858854861292f67b2c4f81d5f55debe1b
m6ttw <matthewrhyswilliams@live.com>
2020-11-27 16:33:04 +0000

    guest1 has a name

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,4 +4,11 @@ from src.guest import Guest
 from src.song import Song
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest1 = Guest("Abbie")
+
+    def test_guest1_has_name(self):
+        self.assertEqual("Abbie", self.guest1.name)
+
+    # def test_guest1_wrong_name_fails(self):
+    #     self.assertEqual("Susan", self.guest1.name)
