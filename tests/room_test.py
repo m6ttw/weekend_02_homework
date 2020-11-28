f1a3210de17466ca681c0b0f31b47f575e8d9237
m6ttw <matthewrhyswilliams@live.com>
2020-11-28 15:48:42 +0000

    guest list working

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,29 +6,41 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
-        self.guest_1 = Guest("Abbie")
-        self.guest_2 = Guest("Bob")
+        self.guest_list = [
+            Guest("Abbie"),
+            Guest("Bob"),
+            Guest("Carol"),
+            Guest("Dave"),
+            Guest("Emma"),
+            Guest("Fred")
+        ]
+
         self.room_1 = Room("Opium")
         self.room_2 = Room("Hive")
-        self.song_list_1 = [
+
+        self.song_list_opium = [
             Song("Mr. Brightside", "The Killers", "Rock"),
-            Song("Juicy", "The Notorious B.I.G.", "Rap"),
-            Song("Dancing Queen", "ABBA", "Pop"),
-            Song("Ring of Fire", "Johnny Cash", "Country"),
-            Song("No Scrubs", "TLC", "R&B"),
-            Song("Fly Me to the Moon", "Frank Sinatra", "Jazz"),
-            Song("Anarchy in the U.K.", "Sex Pistols", "Punk"),
-            Song("No Woman, No Cry", "Bob Marley and the Wailers", "Reggae"),
+            Song("Back in Black", "AC/DC", "Rock"),
+            Song("Walk", "Pantera", "Metal"),
+            Song("Aerials", "System of a Down", "Metal"),
+            Song("Welcome to the Jungle", "Guns N' Roses", "Rock")
+        ]
+
+        self.song_list_hive = [
+            Song("Gold Digger", "Kanye West", "Hip hop"),
+            Song("Shape of You", "Ed Sheeran", "Pop"),
+            Song("Wannabe", "The Spice Girls", "Pop"),
+            Song("I Write Sins Not Tragedies", "Panic! at the Disco", "Pop Punk"),
             Song("Wake Me Up", "Avicii", "Dance")
-            ]
+        ]
 
     def test_room_has_name(self):
         self.assertEqual("Opium", self.room_1.name)
 
     def test_can_add_song_to_room(self):
-        self.room_1.add_song(self.song_list_1[0])
+        self.room_1.add_song(self.song_list_opium[0])
         self.assertEqual(1, self.room_1.song_count())
 
     def test_can_add_guest_to_room(self):
-        self.room_1.add_guest(self.guest_1)
+        self.room_1.add_guest(self.guest_list[0])
         self.assertEqual(1, self.room_1.guest_count())
